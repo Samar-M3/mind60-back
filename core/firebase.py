8bfbb889582fbfb5d5d9e8014eb7eb4b0ff7f154
Samar-M3 <gigachigga81@gmail.com>
2026-03-29 10:39:08 +0545

    asdfghj

--- core/firebase.py
+++ core/firebase.py
@@ -6,7 +6,7 @@ import os
 import firebase_admin
 from firebase_admin import auth, credentials, firestore
 
-from backend.core.config import settings
+from core.config import settings
 
 
 def init_firebase():
